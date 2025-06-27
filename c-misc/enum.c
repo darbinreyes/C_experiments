354bda8fc29498c9c4e223b2c41e4bfbd5fd005c
Darbin Reyes <darbin.e.reyes@gmail.com>
2025-06-26 21:25:58 -0700

    idk

--- c-misc/enum.c
+++ c-misc/enum.c
@@ -3,10 +3,11 @@
 enum {
 	x = 0,
 	y = 0xFFFFFFFF00000000
+	//z = 0x1FFFFFFFF00000000
 } foo;
 
 enum {
-	z
+	a
 } bar;
 
 int main(void) {
@@ -14,6 +15,17 @@ int main(void) {
 	printf("sizeof(foo) = %lu\n", sizeof(foo));
 
 	printf("sizeof(bar) = %lu\n", sizeof(bar));
+
+	int i = 0xDEADBEEF;
+	char k = 0;
+
+	k = i;
+	i = k;
+	printf("i = %x, sizeof(i) = %lu\n", i, sizeof(i));
+	printf("k = %x, sizeof(k) = %lu\n", k, sizeof(k));
+	//int32_t i = y;
+
+	//printf("i = %x\n", i);
 
 	return 0;
 }
